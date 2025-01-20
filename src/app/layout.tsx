a17bbe96b92c9bad63dd3195a0a0b685f0c2d37d
sezaru-dev <cezarbaguhin20@gmail.com>
2025-01-20 11:51:09 +0800

    trying to fix next-theme, and ThemeProvider module error when I try to deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Josefin_Sans } from 'next/font/google'
 import "./globals.css";
-import { ThemeProvider } from "@/providers/ThemeProvider";
+import { ThemeProvider } from "../providers/ThemeProvider";
 
 const JosefinSans = Josefin_Sans({
   weight: ['100', '200','300','400','500','600','700'],
